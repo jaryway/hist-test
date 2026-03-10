2d0165ee15892c4dc579c5f198c40003fd0cee34
jaryway <weizengxun@live.cn>
2026-03-10 12:53:26 +0800

    update pin

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -169,41 +169,41 @@ int _write(int file, char *ptr, int len)
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
 
-    /* USER CODE BEGIN 1 */
+  /* USER CODE BEGIN 1 */
 
-    /* USER CODE END 1 */
+  /* USER CODE END 1 */
 
-    /* MCU Configuration--------------------------------------------------------*/
+  /* MCU Configuration--------------------------------------------------------*/
 
-    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-    HAL_Init();
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-    /* USER CODE BEGIN Init */
+  /* USER CODE BEGIN Init */
 
-    /* USER CODE END Init */
+  /* USER CODE END Init */
 
-    /* Configure the system clock */
-    SystemClock_Config();
+  /* Configure the system clock */
+  SystemClock_Config();
 
-    /* USER CODE BEGIN SysInit */
+  /* USER CODE BEGIN SysInit */
 
-    /* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-    /* Initialize all configured peripherals */
-    MX_DMA_Init();
-    MX_GPIO_Init();
-    MX_TIM3_Init();
-    MX_USART1_UART_Init();
-    MX_USART2_UART_Init();
-    MX_USART3_UART_Init();
-    MX_TIM2_Init();
-    /* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_DMA_Init();
+  MX_GPIO_Init();
+  MX_TIM3_Init();
+  MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
+  MX_USART3_UART_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
 
     printf("System start\r\n");
 
@@ -283,118 +283,121 @@ int main(void)
 
     // 5、开始运动
     printf(">>> Starting motion at 200 RPM\r\n");
-    motor_pwm_move(&motor_pwm, 1000000);
+    motor_pwm_move(&motor_pwm, 10000);
 
-    HAL_Delay(5000); // 等待5秒
+    // HAL_Delay(5000); // 等待5秒
 
-    // 6、运行中改变速度到 2000 RPM
-    printf(">>> Changing speed to 2000 RPM (should take ~3 sec)\r\n");
-    uint32_t change_start = HAL_GetTick();
-    motor_pwm_set_speed(&motor_pwm, speed_max);
+    // // 6、运行中改变速度到 2000 RPM
+    // printf(">>> Changing speed to 2000 RPM (should take ~3 sec)\r\n");
+    // uint32_t change_start = HAL_GetTick();
+    // motor_pwm_set_speed(&motor_pwm, speed_max);
 
-    HAL_Delay(5000); // 等待8秒
+    // HAL_Delay(5000); // 等待8秒
 
-    printf(">>> Changing speed to 100 RPM (should take ~3 sec)\r\n");
-    change_start = HAL_GetTick();
-    motor_pwm_set_speed(&motor_pwm, speed_mid);
+    // printf(">>> Changing speed to 100 RPM (should take ~3 sec)\r\n");
+    // change_start = HAL_GetTick();
+    // motor_pwm_set_speed(&motor_pwm, speed_mid);
 
-    HAL_Delay(3000);
-    change_start = HAL_GetTick();
-    motor_pwm_set_speed(&motor_pwm, 1000.0f / 60.0f * SPR);
+    // HAL_Delay(3000);
+    // change_start = HAL_GetTick();
+    // motor_pwm_set_speed(&motor_pwm, 1000.0f / 60.0f * SPR);
 
-    HAL_Delay(3000);
-    change_start = HAL_GetTick();
-    motor_pwm_set_speed(&motor_pwm, 1500.0f / 60.0f * SPR);
+    // HAL_Delay(3000);
+    // change_start = HAL_GetTick();
+    // motor_pwm_set_speed(&motor_pwm, 1500.0f / 60.0f * SPR);
 
-    HAL_Delay(3000);
-    change_start = HAL_GetTick();
-    motor_pwm_set_speed(&motor_pwm, 500.0f / 60.0f * SPR);
+    // HAL_Delay(3000);
+    // change_start = HAL_GetTick();
+    // motor_pwm_set_speed(&motor_pwm, 500.0f / 60.0f * SPR);
 
-    HAL_Delay(3000);
-    change_start = HAL_GetTick();
-    motor_pwm_set_speed(&motor_pwm, 2200.0f / 60.0f * SPR);
+    // HAL_Delay(3000);
+    // change_start = HAL_GetTick();
+    // motor_pwm_set_speed(&motor_pwm, 2200.0f / 60.0f * SPR);
 
-    HAL_Delay(3000);
-    change_start = HAL_GetTick();
-    motor_pwm_set_speed(&motor_pwm, 22.0f / 60.0f * SPR);
+    // HAL_Delay(3000);
+    // change_start = HAL_GetTick();
+    // motor_pwm_set_speed(&motor_pwm, 22.0f / 60.0f * SPR);
 
-    uint32_t change_time = HAL_GetTick() - change_start;
-    printf(">>> Speed change completed in %lu ms\r\n", change_time);
+    // uint32_t change_time = HAL_GetTick() - change_start;
+    // printf(">>> Speed change completed in %lu ms\r\n", change_time);
 
-    // 7、改变目标位置
-    motor_pwm_set_speed(&motor_pwm, 1000.0f / 60.0f * SPR);
-    printf(">>> Changing target to 2000000\r\n");
-    motor_pwm_move_to(&motor_pwm, -1000000);
+    // // 7、改变目标位置
+    // motor_pwm_set_speed(&motor_pwm, 1000.0f / 60.0f * SPR);
+    // printf(">>> Changing target to 2000000\r\n");
+    // motor_pwm_move_to(&motor_pwm, -1000000);
 
-    HAL_Delay(5000);
-    motor_pwm_move_to(&motor_pwm, 1000000);
-    HAL_Delay(5000);
-    // 8、再次改变速度到 500 RPM
-    printf(">>> Changing speed to 500 RPM\r\n");
-    // uint32_t speed_mid = 500.0f / 60.0f * SPR;
-    motor_pwm_set_speed(&motor_pwm, 2000.0f / 60.0f * SPR);
+    // HAL_Delay(5000);
+    // motor_pwm_move_to(&motor_pwm, 1000000);
+    // HAL_Delay(5000);
+    // // 8、再次改变速度到 500 RPM
+    // printf(">>> Changing speed to 500 RPM\r\n");
+    // // uint32_t speed_mid = 500.0f / 60.0f * SPR;
+    // motor_pwm_set_speed(&motor_pwm, 2000.0f / 60.0f * SPR);
 
-    /* USER CODE END 2 */
+  /* USER CODE END 2 */
 
-    /* Infinite loop */
-    /* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
     uint32_t last_print = 0;
     uint8_t m           = 1;
 
     while (1) {
-        /* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-        /* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
 
-        motor_pwm_move_to(&motor_pwm, m == 1 ? -1000000 : 1000000);
-        m = !m;
-        HAL_Delay(5000);
+        // motor_pwm_move_to(&motor_pwm, m == 1 ? -1000000 : 1000000);
+        // m = !m;
+        // HAL_Delay(5000);
 
-        // 每200ms打印一次状态
-        if (HAL_GetTick() - last_print >= 200) {
-            last_print = HAL_GetTick();
-            print_motor_status(&motor_pwm);
-        }
+        // // 每200ms打印一次状态
+        // if (HAL_GetTick() - last_print >= 200) {
+        //     last_print = HAL_GetTick();
+        //     print_motor_status(&motor_pwm);
+        // }
 
-        HAL_Delay(10);
+        // HAL_Delay(10);
     }
-    /* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /** Initializes the RCC Oscillators according to the specified parameters
-     * in the RCC_OscInitTypeDef structure.
-     */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
-    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-    RCC_OscInitStruct.HSIState       = RCC_HSI_ON;
-    RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLMUL     = RCC_PLL_MUL9;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-        Error_Handler();
-    }
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-    /** Initializes the CPU, AHB and APB buses clocks
-     */
-    RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
-        Error_Handler();
-    }
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
@@ -402,31 +405,31 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
-    /* USER CODE BEGIN Error_Handler_Debug */
+  /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1) {
     }
-    /* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-    /* USER CODE BEGIN 6 */
+  /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-    /* USER CODE END 6 */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
